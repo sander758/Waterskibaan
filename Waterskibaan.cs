a87b505704bad9d4d55555d0b3770a2b8fdb2c9e
sander758 <sander.elderman@home.nl>
2019-10-10 22:44:21 +0200

    Opdracht 4

---
+++ Waterskibaan.cs
@@ -0,0 +1,27 @@
+﻿namespace Waterskibaan
+{
+    public class Waterskibaan
+    {
+        private LijnenVoorraad _lijnenVoorraad = new LijnenVoorraad();
+        private Kabel _kabel = new Kabel();
+
+        public Waterskibaan()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                _lijnenVoorraad.LijnToevoegenAanRij(new Lijn());
+            }
+        }
+
+        public void VerplaatsKabel()
+        {
+            _kabel.VerschuifLijnen();
+            _lijnenVoorraad.LijnToevoegenAanRij(_kabel.VerwijderLijnVanKabel());
+        }
+
+        public override string ToString()
+        {
+            return "De waterskibaan heeft " + _lijnenVoorraad + " op de kabel " + _kabel;
+        }
+    }
+}
